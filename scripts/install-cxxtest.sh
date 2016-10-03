3998fc6e5a46cb467c6bc614a91c9410d311eaa3
Andreas Stallinger <astallinger@gmail.com>
2016-10-03 10:06:12 +0200

    centos-qt

--- scripts/install-cxxtest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git clone https://github.com/CxxTest/cxxtest.git && \
-cd cxxtest && \
-git checkout 4.4
--- scripts/install-ninja.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/martine/ninja.git && \
-cd ninja && \
-git checkout release && \
-./configure.py --bootstrap && \
-mv ninja /usr/bin/ && \
-cd .. && rm -rf ninja
